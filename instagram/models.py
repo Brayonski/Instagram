735ad2d33236985f06d696f44ddd69ed7ff44e0b
root <root@localhost.localdomain>
2018-10-04 11:05:53 +0300

    Image & Profile models

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'media/')
+    image_name = models.CharField(max_length=50)
+    image_caption = models.TextField()
+    profile = models.ForeignKey(Profile)
+    likes = models.TextField()
+    comments = models.TextField()
+
+    # implement methods
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'media')
+    bio = models.TextField()
