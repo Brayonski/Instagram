eeba2f689c31b02d731cb9d0203413005e5c04dc
root <root@localhost.localdomain>
2018-10-05 11:43:50 +0300

    email configuration

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import NewsLetterForm
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 def welcome(request):
@@ -10,7 +11,11 @@ def instagram(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['Brian Mutai']
+            email = form.cleaned_data['kiprotichbrian8@gmail.com']
+            recipient = NewsLetterForm(name = name, email = email)
+            recipient.save()
+            HttpResponseRedirect('instagram')
     else:
         form = NewsLetterForm()
     return render(request, 'all-snaps/instagram.html', {"letterForm":form})
