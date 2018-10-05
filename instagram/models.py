b271857a018d054efe1fc8ad116093b5cbbc6744
root <root@localhost.localdomain>
2018-10-05 11:36:15 +0300

    mail sending set up

--- instagram/models.py
+++ instagram/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 
 # Create your models here.
-
+class NewsLetterRecipent(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to = 'media')
+    profile_photo = models.ImageField(upload_to = 'media/')
     bio = models.TextField()
+
+class Comment(models.Model):
+    comment = models.TextField()
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'media/')
     image_name = models.CharField(max_length=50)
     image_caption = models.TextField()
     profile = models.ForeignKey(Profile)
+    comments = models.ForeignKey(Comment)
     likes = models.TextField()
-    comments = models.TextField()
+
+
 
     # implement methods
--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import NewsLetterForm
 
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
 
-# @login_required(login_url='/accounts/login')
+def instagram(request):
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-snaps/instagram.html', {"letterForm":form})
